e7e840757289412d80f7bb197a25b3bc4f14fa11
Nicholas Kerker <nkerker@protonmail.com>
2025-02-15 11:11:39 -0600

    initial commit

---
+++ myfirstprogram.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World!\n\n");
+  printf("I am learning C.\n");
+
+  int myNum = 15;
+
+  printf("My favorite number is: %d", myNum);
+
+  return 0;
+}
